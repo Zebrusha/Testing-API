f1c78310bcef3a0498718edcbf84971d3db0b52f
Anastasia Voronchikhina <zebrushaa@mail.ru>
2024-04-13 17:22:51 +0300

    Add validation logic

--- src/test/java/ru/netology/rest/MobileBankApiTestV2.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV2 {
@@ -18,9 +19,8 @@ class MobileBankApiTestV2 {
       // Проверки
       .then()
           .statusCode(200)
-          // .header("Content-Type", "application/json; charset=UTF-8")
+          .header("Content-Type", "application/json; charset=UTF-8")
           // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
       ;
     }
 }
